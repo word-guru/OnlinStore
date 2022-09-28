2be3802e01a85a770ec537da2cae0c98cfcc2289
Pavel Popov <legeon48@mail.ru>
2022-09-28 22:00:36 +0300

    Update swagger

--- OnlinStore/Catalog.cs
+++ OnlinStore/Catalog.cs
@@ -4,11 +4,34 @@ namespace OnlinStore;
 
 public class Catalog
 {
-    public List<Product> Products { get; set; } = new()
+    private List<Product> Products { get; set; } = new()
     {
         new Product(1, "Чистый код", 123),
         new Product(2, "Lift", 231),
         new Product(3, "Maus", 312)
     };
+    private readonly object _syncObj = new();
 
+    public void AddProduct(Product product)
+    {
+        lock (_syncObj)
+        {
+            Products.Add(product);
+        }
+    }
+    public void ClearProduct()
+    {
+        lock (_syncObj)
+        {
+            Products.Clear();
+        }
+    }
+
+    public List<Product> GetProducts()
+    {
+        lock (_syncObj)
+        {
+            return Products;
+        }
+    }
 }
--- OnlinStore/Program.cs
+++ OnlinStore/Program.cs
@@ -6,25 +6,31 @@ using OnlinStore.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<JsonOptions>(options => { options.SerializerOptions.WriteIndented = true; }); //настройка вывода JSON
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 var catalog = new Catalog();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
 app.MapGet("/catalog", () =>
 {
-    return catalog.Products;
+    return catalog.GetProducts();
 });
 
 app.MapPost("/catalog/add_product", (Product product, HttpContext context) =>
 {
-    catalog.Products.Add(product);
+    catalog.AddProduct(product);
     context.Response.StatusCode = 201;
 });
 
 app.MapPost("/catalog/clear_product", (HttpContext context) =>
 {
-    catalog.Products.Clear();
+    catalog.ClearProduct();
     context.Response.StatusCode = 202;
 });
 
