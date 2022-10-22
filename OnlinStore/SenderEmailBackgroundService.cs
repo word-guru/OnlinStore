5d3b939c1aafc189b41ee1adef3e294d5eac8f1d
Pavel Popov <legeon48@mail.ru>
2022-10-22 09:14:13 +0300

    Added time delay between sending messages

--- OnlinStore/SenderEmailBackgroundService.cs
+++ OnlinStore/SenderEmailBackgroundService.cs
@@ -1,4 +1,9 @@
-﻿namespace OnlinStore;
+﻿using Org.BouncyCastle.Asn1.Cms;
+using Org.BouncyCastle.Bcpg;
+using Polly;
+using Polly.Retry;
+
+namespace OnlinStore;
 
 public class SenderEmailBackgroundService : BackgroundService
 {
@@ -14,33 +19,87 @@ public class SenderEmailBackgroundService : BackgroundService
         _logger = logger;
         applicationLifetime.ApplicationStarted.Register(() => {_logger.LogInformation("The server has been successfully started");});
     }
-
+    
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-       await using var scope = _serviceProvider.CreateAsyncScope();
+        await using var scope = _serviceProvider.CreateAsyncScope();
         var scopeCurrentTime = scope.ServiceProvider.GetRequiredService<IClock>();
         var scopeSendMessage = scope.ServiceProvider.GetRequiredService<IEmailSender>();
-        var to = "legeon48@mail.ru";
-
+        
         Console.WriteLine("Server started successfully at " + scopeCurrentTime.GetUTCTime());
 
         while (!stoppingToken.IsCancellationRequested)
         {
-            try
-            {
-                await scopeSendMessage.SendAsync("PV011",to,"Server testing",
-                    "Server is working properly" +
-                    " Total memory: " + 
-                    GC.GetTotalMemory(false) + " bytes"
+            var to = "legeon48@mail.ru";
+            const int retryCount = 2;
+
+            AsyncRetryPolicy policy = Policy.Handle<Exception>()
+                .WaitAndRetryAsync( 5, retryAttempt =>
+                    TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    (exception, timeSpan, retryAttempt, context) => {
+                        _logger.LogWarning(exception, "Error while sending email. Retrying: {Attempt}", retryAttempt);
+                    }
                 );
-            }
-            catch (Exception e)
+
+            PolicyResult? result = await policy.ExecuteAndCaptureAsync(
+                token => scopeSendMessage.SendAsync("PV011", to, "Server testing",
+                    "Server is working properly" +
+                    " Total memory: " +
+                    GC.GetTotalMemory(false) + " bytes"
+                ), stoppingToken);
+
+            if (result.Outcome == OutcomeType.Failure)
             {
-                _logger.LogError(e,"there was an error when sending the message, service: {Service}, {Recipient}",scopeSendMessage.GetType());
+                _logger.LogError(result.FinalException, "There was an error while sending email");
             }
+
             
             await Task.Delay(new TimeSpan(1, 0, 0));
         }
     }
+
+    /*protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+       await using var scope = _serviceProvider.CreateAsyncScope();
+        var scopeCurrentTime = scope.ServiceProvider.GetRequiredService<IClock>();
+        var scopeSendMessage = scope.ServiceProvider.GetRequiredService<IEmailSender>();
+        
+        Console.WriteLine("Server started successfully at " + scopeCurrentTime.GetUTCTime());
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var to = "legeon48@mail.ru";
+            bool sendingSucceeded = false;
+            const int attemptsLimit = 2;
+            
+            for(int attemptsCount = 1;!sendingSucceeded && attemptsCount <= attemptsLimit;attemptsCount++)
+            {
+                try
+                {
+                    await scopeSendMessage.SendAsync("PV011", to, "Server testing",
+                        "Server is working properly" +
+                        " Total memory: " +
+                        GC.GetTotalMemory(false) + " bytes"
+                    );
+                    sendingSucceeded = true;
+                }
+                catch (Exception e) when (attemptsCount < attemptsLimit)
+                {
+
+                    _logger.LogWarning(e,
+                        "При попытке отправить письмо произошла ошибка, сервис:  {Service}, {Recipient}." +
+                        "Попытка #{Attempt}", scopeSendMessage.GetType(), to, attemptsCount);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e,
+                        "Отправка письма завершилась с ошибкой, сервис: {Service}, {Recipient}." +
+                        "Попытка #{Attempt}", scopeSendMessage.GetType(), to, attemptsCount);
+                }
+            } 
+            
+            await Task.Delay(new TimeSpan(1, 0, 0));
+        }
+    }*/
     
 }
