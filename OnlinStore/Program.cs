ddd89d49f74383ed1aabb94ea6d2f50fb8a3d63e
Pavel Popov <legeon48@mail.ru>
2022-10-22 10:49:59 +0300

    Add Serilog

--- OnlinStore/Program.cs
+++ OnlinStore/Program.cs
@@ -4,50 +4,81 @@ using Microsoft.AspNetCore.Http.Json;
 using OnlinStore;
 using OnlinStore.Interface;
 using OnlinStore.Models;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.Configure<JsonOptions>(options => { options.SerializerOptions.WriteIndented = true; }); //настройка вывода JSON
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<ICatalog, InMemoryCatalog>();
-builder.Services.AddSingleton<IClock,TimeInUTC>();
-builder.Services.Configure<SmtpConfig>(builder.Configuration.GetSection("SmtpConfig"));
-builder.Services.AddScoped<IEmailSender, MailKitEmailSender>();
-builder.Services.AddHostedService<SenderEmailBackgroundService>();
+Log.Information("Starting up");
 
-var app = builder.Build();
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Services.Configure<JsonOptions>(options => { options.SerializerOptions.WriteIndented = true; }); //настройка вывода JSON
 
-app.UseSwagger();
-app.UseSwaggerUI();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddSingleton<ICatalog, InMemoryCatalog>();
+    builder.Services.AddSingleton<IClock,TimeInUTC>();
+    builder.Services.Configure<SmtpConfig>(builder.Configuration.GetSection("SmtpConfig"));
+//builder.Services.Configure<SmtpConfig>(AddUserSecrets.ConfigureAppConfiguration);
+    builder.Services.AddScoped<IEmailSender, MailKitEmailSender>();
+    builder.Services.AddHostedService<SenderEmailBackgroundService>();
+    builder.Host.UseSerilog((ctx, conf) =>
+    {
+        conf
+            .WriteTo.Console()
+            .WriteTo.File("log-.txt", rollingInterval: RollingInterval.Day)
+            .ReadFrom.Configuration(ctx.Configuration)
+            ;
+    });
+
+
+    var app = builder.Build();
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseSerilogRequestLogging();
 
 //var catalog = app.Services.GetService<ICatalog>();
 
-async Task<string> SendMail(IEmailSender sender, string message, string subject)
-{
-    await sender.SendAsync("PV011", "legeon48@mail.ru", subject, message);
+    async Task<string> SendMail(IEmailSender sender, string message, string subject)
+    {
+        await sender.SendAsync("PV011", "legeon48@mail.ru", subject, message);
 
-    return "Ok";
+        return "Ok";
+    }
+
+    app.MapGet("/sendmail", SendMail);
+
+    app.MapGet("/catalog", (ICatalog catalog) =>
+    {
+        return catalog.GetProducts();
+    });
+
+    app.MapPost("/catalog/add_product", (ICatalog catalog,Product product, HttpContext context) =>
+    {
+        catalog.AddProduct(product);
+        context.Response.StatusCode = 201;
+    });
+
+    app.MapPost("/catalog/clear_product", (ICatalog catalog,HttpContext context) =>
+    {
+        catalog.ClearProduct();
+        context.Response.StatusCode = 202;
+    });
+    app.MapGet("/time", (IClock time) => time.GetUTCTime());
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush(); //перед выходом дожидаемся пока все логи будут записаны
 }
 
-app.MapGet("/sendmail", SendMail);
-
-app.MapGet("/catalog", (ICatalog catalog) =>
-{
-    return catalog.GetProducts();
-});
-
-app.MapPost("/catalog/add_product", (ICatalog catalog,Product product, HttpContext context) =>
-{
-    catalog.AddProduct(product);
-    context.Response.StatusCode = 201;
-});
-
-app.MapPost("/catalog/clear_product", (ICatalog catalog,HttpContext context) =>
-{
-    catalog.ClearProduct();
-    context.Response.StatusCode = 202;
-});
-app.MapGet("/time", (IClock time) => time.GetUTCTime());
-
-app.Run();
